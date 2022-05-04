607df59818017d48b260e609526d5ba9b3af27ef
Akash Hegde <akashhegde2012@gmail.com>
2022-05-04 11:16:27 +0530

    port

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express=require('express'),
 	app  = express();
-	
+const PORT = process.env.PORT || 8080
 app.get('/',(req,res)=>{
 	res.send('<h1>hello updated again sanath</h1>');
 	}
 );
-app.listen(4000,()=>{
-	console.log('4000');
+app.listen(PORT,()=>{
+	console.log('8080');
 	});
 	
