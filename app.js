dfa09b69752c1dcd254dd52ba536bc1af2a05fe8
Akash Hegde <akashhegde2012@gmail.com>
2022-05-04 16:54:29 +0530

    env

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express=require('express'),
 	app  = express();
-const PORT = process.env.PORT || 8080
+
+const PORT = process.env.PORT 
 app.get('/',(req,res)=>{
 	res.send('<h1>hello updated again sanath</h1>');
 	}
