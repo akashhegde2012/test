8225fc757a73651432031fd19b1e93981467ee0d
Akash Hegde <akashhegde2012@gmail.com>
2022-05-28 11:31:31 +0530

    mysql_update

--- app.js
+++ app.js
@@ -17,12 +17,16 @@ con.connect(function(err) {
   });
 const PORT = process.env.PORT || 8080
 app.get('/',async (req,res)=>{
+
+	res.send('<h1>hello updated again sanath</h1>');
+	}
+);
+app.get('/mysql' ,async (req,res)=>{
 	await con.query('select * from t1',(err,rows,fields)=>{
         res.status(201).send(rows);
      });
-	// res.send('<h1>hello updated again sanath</h1>');
-	}
-);
+    });
+    
 app.listen(PORT,()=>{
 	console.log('8080');
 	});
