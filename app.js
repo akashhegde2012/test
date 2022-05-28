a868a7937241643d826488c83013f9cdb0bf977b
Akash Hegde <akashhegde2012@gmail.com>
2022-05-28 22:44:40 +0530

    mysql -rm

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const express=require('express'),
 app.use(cors());
 
 
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "root",
-//   database:'akash',
-//   port:12254,
-// });
-// con.connect(function(err) {
-// 	console.log("Connected!");
-//   });
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database:'akash',
+});
+con.connect(function(err) {
+	console.log("Connected!");
+  });
 const PORT = process.env.PORT || 8080
 app.get('/',async (req,res)=>{
 
