e85e12dbc8e5dc6a86cf1fd6eb2608fc19dfd355
Akash Hegde <akashhegde2012@gmail.com>
2022-06-02 10:50:48 +0530

    mongo

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ require('dotenv').config();
 const express=require('express'),
 	app  = express(),
 	mysql = require('mysql'),
+  mongoose = require('mongoose'),
+  User = require('./models/user')
   cors = require('cors');
 app.use(cors());
 
+mongoose.connect("mongodb://localhost:27018/testing");
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database:'akash',
-});
-con.connect(function(err) {
-	console.log("Connected!");
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database:'akash',
+//   connectionLimit: 10,
+// });
+// con.connect(function(err) {
+// 	console.log("Connected!");
 
 const PORT = process.env.PORT || 8080
 app.get('/',async (req,res)=>{
@@ -21,17 +25,23 @@ app.get('/',async (req,res)=>{
 	res.send('<h1>hello updated again sanath</h1>');
 	}
 );
-app.get('/mysql' ,async (req,res)=>{
-	 con.query('select * from t1',(err,rows,fields)=>{
-      if (err){
-        console.log(err);
-      }
-        console.log(rows);
-        res.status(201).send(rows);
-     });
+app.get('/users',async (req,res)=>{
+  User.find({},(err,users)=>{
+    console.log(users);
+    res.send(users);
+  })
+})
+app.get('/mongo' ,async (req,res)=>{
+	 var newUser = {
+     name:'akash',
+     email: 'akash@abc.com'
+   }
+   User.create(newUser,(err,user)=>{
+     res.redirect('/users');
+   })
     });
 
 app.listen(PORT,()=>{
 	console.log('8080');
 	});
-});
+
