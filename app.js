3792d505deedf394eebd7a166771004b3b01478c
Akash Hegde <akashhegde2012@gmail.com>
2022-06-05 18:07:15 +0530

    mysql_final

--- app.js
+++ app.js
@@ -10,51 +10,51 @@ const PORT = process.env.PORT || 8080
 
 // mongoose.connect("mongodb://localhost:27018/testing");
 // mongoose.connect("mongodb://127.0.0.1:27018/test_docker", async (err))
-// mongoose.connect("mongodb://172.19.0.1:27018/test_docker",{useNewUrlParser:true,useUnifiedTopology:true})
-//     .then(()=>{
-//         app.listen(PORT,()=>{console.log('Server running in port '+PORT)});
-//     })
-//     .catch((err)=> console.log(err));
-var con = mysql.createConnection({
-  host: "172.19.0.1",
-  // host:'localhost',
-  user: "root",
-  password: "root",
-  database:'akash',
-  port:6035
-});
-con.connect(function(err,con) {
-	console.log("Connected!");
-})
+mongoose.connect("mongodb://172.19.0.1:27792/test_docker",{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=>{
+        app.listen(PORT,()=>{console.log('Server running in port '+PORT)});
+    })
+    .catch((err)=> console.log(err));
+// var con = mysql.createConnection({
+//   host: "172.19.0.1",
+//   // host:'localhost',
+//   user: "root",
+//   password: "root",
+//   database:'akash',
+//   port:6035
+// });
+// con.connect(function(err,con) {
+// 	console.log("Connected!");
+// })
 app.get('/',async (req,res)=>{
 
 	res.send('<h1>hello updated again sanath</h1>');
 	}
 );
-// app.get('/users',async (req,res)=>{
-//   User.find({},(err,users)=>{
-//     console.log(users);
-//     res.send(users);
-//   })
-// })
-// app.get('/mongo' ,async (req,res)=>{
-// 	 var newUser = {
-//      name:'akash',
-//      email: 'akash@abc.com'
-//    }
-//    User.create(newUser,(err,user)=>{
-//      res.redirect('/users');
-//    })
-//     });
-app.get('/mysql' ,async (req,res)=>{
-    con.query('select * from t1',(err,rows,fields)=>{
-      if (err){
-        console.log(err);
-      }
-        console.log(rows);
-        res.status(201).send(rows);
-       });
+app.get('/users',async (req,res)=>{
+  User.find({},(err,users)=>{
+    console.log(users);
+    res.send(users);
   })
+})
+app.get('/mongo' ,async (req,res)=>{
+	 var newUser = {
+     name:'akash',
+     email: 'akash@abc.com'
+   }
+   User.create(newUser,(err,user)=>{
+     res.redirect('/users');
+   })
+    });
+// app.get('/mysql' ,async (req,res)=>{
+//     con.query('select * from t1',(err,rows,fields)=>{
+//       if (err){
+//         console.log(err);
+//       }
+//         console.log(rows);
+//         res.status(201).send(rows);
+//        });
+//   })
 
 
 app.listen(PORT,()=>{
